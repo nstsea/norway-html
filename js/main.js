f5fd5b84eaff58027dc143fa9d2137d564b80136
nst.see <av9601@mail.ru>
2023-06-30 22:22:41 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// header sliedr //
+
+const slider = tns({
+    container: '.header-slider',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    controls: false,
+    autoplayButtonOutput: false,
+  });
